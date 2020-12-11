2d17491cb0db595df42511fc0f26b281553d342b
eugene <eugene.kozhukalo@ordergroove.com>
2020-12-11 10:08:05 -0500

    DATA-2495 fix python2 incompatible syntax, support for cross-schema queries

--- pybigquery/sqlalchemy_bigquery.py
+++ pybigquery/sqlalchemy_bigquery.py
@@ -130,6 +130,9 @@ class BigQueryExecutionContext(DefaultExecutionContext):
 
 
 class BigQueryCompiler(SQLCompiler):
+
+    use_schema_names_for_columns = False
+
     def __init__(self, dialect, statement, column_keys=None,
                  inline=False, **kwargs):
         if isinstance(statement, Column):
@@ -174,7 +177,7 @@ class BigQueryCompiler(SQLCompiler):
         else:
             effective_schema = self.preparer.schema_for_object(table)
 
-            if effective_schema:
+            if self.use_schema_names_for_columns and effective_schema:
                 schema_prefix = self.preparer.quote_schema(
                     effective_schema) + '.'
             else:
@@ -186,18 +189,19 @@ class BigQueryCompiler(SQLCompiler):
                 self.preparer.quote(tablename) + \
                 "." + name
 
-    def visit_label(self, *args, within_group_by=False, **kwargs):
+    def visit_label(self, *args, **kwargs):
         # Use labels in GROUP BY clause.
         #
         # Flag set in the group_by_clause method. Works around missing
         # equivalent to supports_simple_order_by_label for group by.
+        within_group_by = kwargs.get('within_group_by', False)
         if within_group_by:
             kwargs['render_label_as_label'] = args[0]
         return super(BigQueryCompiler, self).visit_label(*args, **kwargs)
 
     def group_by_clause(self, select, **kw):
         return super(BigQueryCompiler, self).group_by_clause(
-            select, **kw, within_group_by=True
+            select, within_group_by=True, **kw
         )
 
 
